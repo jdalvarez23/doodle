934cf588920a1b9ee3e315daa82180b6b8668cfc
Jose D. Alvarez <jdalvarez23@gmail.com>
2020-06-12 22:28:36 -0500

    Added styling to index page

--- index.php
+++ index.php
@@ -2,25 +2,21 @@
 <html>
     <head>
         <title>Welcome to Doodle</title>
-
         <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     </head>
     <body>
-        
-        <div class="mainSection">
-
-            <div class="logoContainer">
-                <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+        <div class="wrapper indexPage">
+            <div class="mainSection">
+                <div class="logoContainer">
+                    <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+                </div>
+                <div class="searchContainer">
+                    <form action="search.php" method="GET">
+                        <input class="searchBox" type="text" name="q">
+                        <input class="searchButton" type="submit" value="Search">
+                    </form>
+                </div>
             </div>
-
-            <div class="searchContainer">
-                <form action="search.php" method="GET">
-                    <input class="searchBox" type="text" name="q">
-                    <input class="searchButton" type="submit" value="Search">
-                </form>
-            </div>
-
         </div>
-
     </body>
 </html>
